99c8160af4f3285738c1386c9e78f73870e1084d
apsh0tDev <apsh0t@proton.me>
2024-06-12 18:05:01 -0400

    gitio

--- uploader.py
+++ uploader.py
@@ -48,7 +48,7 @@ async def upload_to_supabase(ics, txt):
 
 async def getPublicUrl(file_path):
      data = supabase.storage.from_(bucket_name).get_public_url(file_path)
-     url = pyshorteners.Shortener().owly.short(data)
+     url = pyshorteners.Shortener().gitio.short(data)
      return url
          
 #DEPRECATED
