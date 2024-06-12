d14d8d91ec929b662470995feb2b17154d848101
apsh0tDev <apsh0t@proton.me>
2024-06-12 17:54:44 -0400

    shortener

--- uploader.py
+++ uploader.py
@@ -12,7 +12,6 @@ email = os.getenv("SUPABASE_EMAIL")
 password = os.getenv("SUPABASE_PASSWORD")
 supabase: Client = create_client(url, key)
 bucket_name = "calendars"
-type_tiny = pyshorteners.Shortener()
 
 async def upload_to_supabase(ics, txt):
     ics_exists = os.path.exists(ics)
@@ -49,7 +48,7 @@ async def upload_to_supabase(ics, txt):
 
 async def getPublicUrl(file_path):
      data = supabase.storage.from_(bucket_name).get_public_url(file_path)
-     url = type_tiny.tinyurl.short(data)
+     url = pyshorteners.Shortener().clckru.short(data)
      return url
          
 #DEPRECATED
